46c935eebb2e547f73f9a40698d6d2c6c847d3d1
KatyaChirkova <m2404967@edu.misis.ru>
2026-01-04 23:21:09 +0300

    Application слой: интерфейсы OrderRepository и PaymentGateway, PayOrderUseCase

--- application/interfaces.py
+++ application/interfaces.py
@@ -1,1 +1,28 @@
-﻿# Interfaces: OrderRepository, PaymentGateway
+﻿from abc import ABC, abstractmethod
+from typing import Optional
+from domain.entities import Order
+from domain.value_objects import Money
+
+class OrderRepository(ABC):
+    """Интерфейс репозитория для работы с заказами"""
+    
+    @abstractmethod
+    def get_by_id(self, order_id: int) -> Optional[Order]:
+        """Найти заказ по ID"""
+        pass
+
+    @abstractmethod
+    def save(self, order: Order) -> None:
+        """Сохранить заказ"""
+        pass
+
+class PaymentGateway(ABC):
+    """Интерфейс платежного шлюза"""
+    
+    @abstractmethod
+    def charge(self, order_id: int, amount: Money) -> bool:
+        """
+        Выполнить платеж.
+        Возвращает True если платеж прошел успешно, False в случае ошибки.
+        """
+        pass
--- application/use_cases.py
+++ application/use_cases.py
@@ -1,1 +1,43 @@
-﻿# Use cases: PayOrderUseCase
+﻿from typing import Tuple
+from .interfaces import OrderRepository, PaymentGateway
+from domain.entities import Order
+from domain.exceptions import DomainException
+
+class PayOrderUseCase:
+    """Use Case: Оплата заказа"""
+    
+    def __init__(
+        self,
+        order_repository: OrderRepository,
+        payment_gateway: PaymentGateway
+    ):
+        self.order_repository = order_repository
+        self.payment_gateway = payment_gateway
+
+    def execute(self, order_id: int) -> Tuple[bool, str]:
+        """
+        Основной метод Use Case.
+        Возвращает кортеж (успех_операции, сообщение).
+        """
+        # 1. Загружаем заказ
+        order = self.order_repository.get_by_id(order_id)
+        if not order:
+            return False, f"Order {order_id} not found"
+        
+        try:
+            # 2. Выполняем доменную операцию (проверка инвариантов)
+            order.pay()
+            
+            # 3. Вызываем внешний платежный сервис
+            payment_success = self.payment_gateway.charge(order.id, order.total_amount)
+            
+            if not payment_success:
+                return False, "Payment gateway declined the transaction"
+            
+            # 4. Сохраняем обновленный заказ
+            self.order_repository.save(order)
+            
+            return True, f"Order {order_id} successfully paid"
+            
+        except DomainException as e:
+            return False, str(e)
