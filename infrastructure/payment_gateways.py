63f8fd75fdb1f193c4ac6dc0894c748a675467c7
KatyaChirkova <m2404967@edu.misis.ru>
2026-01-04 23:22:08 +0300

    Infrastructure слой: реализации InMemoryOrderRepository и FakePaymentGateway

--- infrastructure/payment_gateways.py
+++ infrastructure/payment_gateways.py
@@ -1,1 +1,26 @@
-﻿# Payment gateway implementations
+﻿from application.interfaces import PaymentGateway
+from domain.value_objects import Money
+
+class FakePaymentGateway(PaymentGateway):
+    """
+    Фейковый платежный шлюз для тестирования.
+    """
+    
+    def __init__(self, fail_mode: bool = False):
+        """
+        :param fail_mode: Если True, все платежи будут отклоняться
+        """
+        self.fail_mode = fail_mode
+        self.charges_log = []  # Лог для проверки в тестах
+
+    def charge(self, order_id: int, amount: Money) -> bool:
+        self.charges_log.append((order_id, amount))
+        
+        if self.fail_mode:
+            return False
+        
+        # Фейковая логика: отклоняем платежи больше 10000 рублей
+        if amount.amount > 10000_00:
+            return False
+        
+        return True
--- infrastructure/repositories.py
+++ infrastructure/repositories.py
@@ -1,1 +1,24 @@
-﻿# Repository implementations
+﻿from typing import Dict, Optional
+from application.interfaces import OrderRepository
+from domain.entities import Order
+
+class InMemoryOrderRepository(OrderRepository):
+    """Реализация репозитория в оперативной памяти"""
+    
+    def __init__(self):
+        self._storage: Dict[int, Order] = {}
+        self._next_id = 1
+
+    def get_by_id(self, order_id: int) -> Optional[Order]:
+        return self._storage.get(order_id)
+
+    def save(self, order: Order) -> None:
+        if order.id is None:
+            order.id = self._next_id
+            self._next_id += 1
+        self._storage[order.id] = order
+
+    # Дополнительные методы для тестирования
+    def add_order(self, order: Order) -> None:
+        """Вспомогательный метод для добавления заказа"""
+        self.save(order)
